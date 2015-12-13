323f6e767a146e691a4043c62546f9b1939c22d3
Josef Fazekas <j.fazekas@theuntitled.eu>
2015-12-13 23:32:46 +0100

    New Networking Code

---
+++ Networking/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+	private readonly string PublicGameName = "jf.spielwiese.networking";
+
+	private HostData[] _hostData;
+
+	[Tooltip( "The player prefab" )]
+	public GameObject PlayerPrefab;
+
+	public void StartServer() {
+		Network.InitializeServer( 16 , 25002 , false );
+		MasterServer.RegisterHost( PublicGameName , "Networking Tutorial Server" , "Test implementation of server code." );
+	}
+
+	private void Start() {
+		InvokeRepeating( "RefreshHostList" , 0 , 3 );
+	}
+
+	private void OnServerInitialized() {
+		Debug.Log( "Server initialized" );
+		SpawnPlayer();
+	}
+
+	[HideInInspector]
+	public void OnMasterServerEvent( MasterServerEvent masterServerEvent ) {
+		switch ( masterServerEvent ) {
+
+			case MasterServerEvent.RegistrationFailedGameName:
+				Debug.Log( "RegistrationFailedGameName" );
+				break;
+			case MasterServerEvent.RegistrationFailedGameType:
+				Debug.Log( "RegistrationFailedGameType" );
+				break;
+			case MasterServerEvent.RegistrationFailedNoServer:
+				Debug.Log( "RegistrationFailedNoServer" );
+				break;
+			case MasterServerEvent.RegistrationSucceeded:
+				Debug.Log( "RegistrationSucceeded" );
+				break;
+			case MasterServerEvent.HostListReceived:
+				Debug.Log( "HostListReceived" );
+				break;
+			default:
+				throw new ArgumentOutOfRangeException( "masterServerEvent" , masterServerEvent , null );
+		}
+	}
+
+	private void RefreshHostList() {
+		Debug.Log( "Refreshing" );
+
+		MasterServer.RequestHostList( PublicGameName );
+
+		_hostData = MasterServer.PollHostList();
+
+		if ( _hostData == null || _hostData.Length == 0 ) {
+			Debug.Log( "No active servers found" );
+		} else {
+			Debug.Log( string.Format( "{0} Host(s) found" , _hostData.Length ) );
+		}
+	}
+
+	private void SpawnPlayer() {
+		Network.Instantiate( PlayerPrefab , new Vector3( 0 , 0 , 0 ) , Quaternion.identity , 0 );
+	}
+
+}
